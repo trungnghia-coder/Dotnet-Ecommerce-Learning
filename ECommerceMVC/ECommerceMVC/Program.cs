b4a2af20f24c793c5fcd260f6a28c492b32d9be4
trungnghia-coder <nguyentrungnghia0907@gmail.com>
2026-01-26 11:34:40 +0700

    Learning: Add Ecommerce MVC project

---
+++ ECommerceMVC/ECommerceMVC/Program.cs
@@ -0,0 +1,31 @@
+using ECommerceMVC.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<Hshop2023Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HShop") ?? throw new InvalidOperationException("Connection string 'Hshop2023Context' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
