6a21a5729f4284160203a8ddaa871f8e186d4d3c
trungnghia-coder <nguyentrungnghia0907@gmail.com>
2026-01-26 15:52:29 +0700

    feat: add search method

--- ECommerceMVC/ECommerceMVC/Controllers/MerchandiseController.cs
+++ ECommerceMVC/ECommerceMVC/Controllers/MerchandiseController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceMVC.Controllers
 {
     public class MerchandiseController : Controller
     {
-        public IActionResult Index(int? category)
+        private readonly Hshop2023Context db;
+
+        public MerchandiseController(Hshop2023Context context)
         {
-            return View();
+            db = context;
+        }
+        public IActionResult Index(int? category, string? query)
+        {
+            var merchandises = db.HangHoas.AsQueryable();
+            
+            // Lọc theo category
+            if (category.HasValue)
+            {
+                merchandises = merchandises.Where(p => p.MaLoai == category.Value);
+            }
+
+            // Tìm kiếm theo tên sản phẩm hoặc tên category
+            if (!string.IsNullOrEmpty(query))
+            {
+                merchandises = merchandises.Where(p => 
+                    p.TenHh.Contains(query) || 
+                    p.MaLoaiNavigation.TenLoai.Contains(query));
+                
+                ViewBag.SearchQuery = query;
+            }
+
+            var result = merchandises.Select(p => new MerchandiseVM
+            {
+                MerchandiseId = p.MaHh,
+                Merchandisename = p.TenHh,
+                Price = p.DonGia ?? 0,
+                Image = p.Hinh ?? "",
+                Description = p.MoTaDonVi ?? "",
+                CategoryName = p.MaLoaiNavigation.TenLoai
+            });
+            
+            return View(result);
         }
     }
 }
---
+++ ECommerceMVC/ECommerceMVC/ViewModels/MerchandiseVM.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerceMVC.ViewModels
+{
+    public class MerchandiseVM
+    {
+        public int MerchandiseId { get; set; }
+        public string Merchandisename { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
